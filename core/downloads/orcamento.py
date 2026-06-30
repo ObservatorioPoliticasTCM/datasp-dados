9f6cf6f56963e93042553310fcc6d985b5041047
Bruno Azuma Balzano <41346451+brunoazuma@users.noreply.github.com>
2026-06-30 12:19:54 -0300

    111 saude atualizar arquivos de orcamento (#112)
    * Adicionada correção para anos anteriores a 2024
    
    * Código ajustado para utilizar o novo módulo de download
    
    * Código ajustado para trabalhar com mais de um período
    
    * Adicionados anos de 2024 e 2025
    
    * DEFAULT_HEADERS movido para modulo de downloads
    
    * Criado modulo de download para consultas
    
    * Download de consultas alterado
    
    * Função ajustada para adicionar ano de referência ao dataframe
    
    * Notebook ajustado para vários anos
    
    * Adicionado tratamento a strings com quebra de linha
    
    * Adicionada url de 2025
    
    * Adicionadas consultas de 2025 e chave composta ajustada
    
    * Atualizada versão do notebook com saídas

--- core/downloads/orcamento.py
+++ core/downloads/orcamento.py
@@ -14,21 +14,9 @@ from io import BytesIO
 import pandas as pd
 from logging import info
 
-DEFAULT_HEADERS = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'pt-BR,pt;q=0.9',
-    'Connection': 'keep-alive',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Sec-Fetch-User': '?1',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Mobile Safari/537.36 Edg/142.0.0.0',
-    'sec-ch-ua': '"Chromium";v="142", "Microsoft Edge";v="142", "Not_A Brand";v="99"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"'
-}
+
+from ..downloads import DEFAULT_HEADERS
+
 
 def load_orcamento(year: int,
                    headers: dict = DEFAULT_HEADERS,
@@ -120,6 +108,8 @@ def load_orcamento_r(year: int,
         descriptive message.
     """
     url = f'https://prefeitura.sp.gov.br/cidade/secretarias/upload/seplan/arquivos/Exercicio_{year}/basedadosDA_{year}.csv'
+    if year < 2024:
+        url = f'https://prefeitura.sp.gov.br/cidade/secretarias/upload/seplan/arquivos/Exercicio_{year}/basedadosDA_12{str(year)[-2:]}.csv'
     info(f"Fetching data from URL: {url}")
     try:
         response = requests.get(url, headers=headers, timeout=request_timeout)
